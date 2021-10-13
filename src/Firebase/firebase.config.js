54b025c19bba624e74668945034d8cd3ce231317
asifalam515 <asibulalam515@gmail.com>
2021-10-13 19:16:09 +0600

    project finished

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLbU7_4ok-JvfuShqkKUZ2hzRXOVkzd48",
+  authDomain: "react-firebase-authentic-ecf58.firebaseapp.com",
+  projectId: "react-firebase-authentic-ecf58",
+  storageBucket: "react-firebase-authentic-ecf58.appspot.com",
+  messagingSenderId: "109175508752",
+  appId: "1:109175508752:web:dcfece4cbd0d7d38225f95"
+};
+export default firebaseConfig;
